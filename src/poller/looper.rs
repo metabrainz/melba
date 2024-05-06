9c40b69f39432323785c89c1cbe8d5cca05ced6a
yellowhatpro <yellowhatpro3119@gmail.com>
2024-05-06 14:23:00 +0530

    wip:utils added for edit_data, edit_note, async errors fixed, added musicbrainz_db conn pool

--- src/poller/looper.rs
+++ src/poller/looper.rs
@@ -1,16 +1,32 @@
-use sqlx::{Error, Row};
+use mb_rs::schema::{EditData, EditNote};
+use sqlx::{Error};
+use crate::poller::utils::{extract_url_from_edit_data, extract_urls_from_edit_note};
 
 pub async fn poll_db(
     pool: &sqlx::PgPool
 ) -> Result<(), Error> {
-    //TODO: perform the edit_note and edit_data
-    let rows = sqlx::query(
+    let edits = sqlx::query_as::<_, EditData>(
         "SELECT * FROM edit_data LIMIT 10")
         .fetch_all(pool)
         .await?;
+
+    let notes = sqlx::query_as::<_, EditNote>(
+        "SELECT * FROM edit_note LIMIT 10")
+        .fetch_all(pool)
+        .await?;
+
     //TODO: transformations, and save transformed data to internet_archive_urls
-    for row in rows {
-        println!("{:?}", row.columns());
+    println!("Edits ->");
+    for edit in edits {
+        let extracted_data = extract_url_from_edit_data(edit.data);
+        if extracted_data.is_some() {
+            println!("{}", extracted_data.unwrap());
+        }
+    }
+    println!("Edit Notes ->");
+    for note in notes {
+        let urls = extract_urls_from_edit_note(note.text.as_str());
+        println!("{:?}", urls);
     }
     Ok(())
 }
--- src/poller/utils.rs
+++ src/poller/utils.rs
@@ -1,13 +1,17 @@
-use regex::Regex;
+use linkify::{LinkFinder, LinkKind};
+use serde_json::json;
+use sqlx::types::JsonValue;
 
 /// This function takes text from edit note and outputs a vector of URLs as string
 pub fn extract_urls_from_edit_note(note: &str) -> Vec<String> {
-    //TODO: test regex on more URLs
-    //Used regex mentioned here: https://stackoverflow.com/a/29288898/15084244
-    let re = Regex::new(r"(?:(?:https?|ftp|file):\/\/|www\.|ftp\.)(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[-A-Z0-9+&@#\/%=~_|$?!:,.])*(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[A-Z0-9+&@#\/%=~_|$])").unwrap();
-    re.find_iter(note)
-        .map(|mat| mat.as_str().to_string())
-        .collect()
+    let mut finder = LinkFinder::new();
+    finder.kinds(&[LinkKind::Url]);
+
+    let mut urls: Vec<_> = finder
+        .links(note)
+        .map(|link|{link.as_str().to_string()})
+        .collect();
+    urls
 }
 
 /// This function takes input a URL string, and returns true if it should exclude the URL from saving
@@ -15,4 +19,21 @@ pub fn should_exclude_url(url: &str) -> bool {
     // TODO: discuss and add keywords to identify URLs we want to exclude
     let keywords: Vec<String> = vec![];
     keywords.iter().any(|keyword| url.contains(keyword))
+}
+
+/// This function takes input Edit Data in form of JSONValue, checks if the Edit Data contains URL, and returns the URL as String
+pub fn extract_url_from_edit_data(json: JsonValue) -> Option<String> {
+    return if json.get("type1") == Some(&json!("url")) {
+        // Edit type: Add Relationship
+        let entity0 = json.get("entity1").unwrap();
+        Some(entity0.get("name").unwrap().to_string())
+    } else if json.get("new").is_some() && json.get("new").unwrap().is_object() {
+        //Edit type: Edit URL
+        let new = json.get("new").unwrap();
+        return if new.get("url").is_some() && new.get("url") != Some(&json!(null)) {
+            Some(new.get("url").unwrap().to_string())
+        } else { None }
+    } else {
+        None
+    }
 }

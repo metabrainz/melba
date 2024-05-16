610b77b7454f817d07e833c36fadddee1e9f880c
yellowhatpro <yellowhatpro3119@gmail.com>
2024-05-16 21:50:36 +0530

    added - internet_archive_urls table,added- increment records of edit_note and edit_table on each poll, wip: insert the urls to internet_archive_urls table fetched from edit* tables

---
+++ src/poller/internet_archive_urls.rs
@@ -0,0 +1,17 @@
+#![allow(dead_code)]
+// Generated with sql-gen
+// https://github.com/jayy-lmao/sql-gen
+//TODO: move this to mb-rs
+use sqlx::types::chrono;
+
+#[derive(sqlx::FromRow, Debug)]
+pub struct InternetArchiveUrls {
+    pub id: i32,
+    pub url: Option<String>,
+    pub job_id: Option<String>,
+    pub from_table: Option<String>,
+    pub from_table_id: Option<i32>,
+    pub created_at: Option<chrono::DateTime<chrono::Utc>>,
+    pub retry_count: Option<i32>,
+    pub is_saved: Option<bool>,
+}

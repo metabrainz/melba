e4a404bd7f4a609b88e67ebc4ece74bdb2c24a49
yellowhatpro <yellowhatpro3119@gmail.com>
2024-06-21 12:27:07 +0530

    init tests

---
+++ tests/fixtures/EditNote.sql
@@ -0,0 +1,11 @@
+CREATE TABLE edit_note (
+    id                  SERIAL,
+    editor              INTEGER NOT NULL, -- references editor.id
+    edit                INTEGER NOT NULL, -- references edit.id
+    text                TEXT NOT NULL,
+    post_time            TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+);
+
+INSERT INTO edit_note(editor, edit, text) VALUES
+(291680, 111451706, 'Same recording; one has incorrect duration'),
+(2274995, 111451711, 'https://seraphitus-seraphita.bandcamp.com/album/cosmic-horrors-iii * https://f4.bcbits.com/img/a2736839660_10.jpg → Maximised to https://f4.bcbits.com/img/a2736839660_0.jpg -  MB: Enhanced Cover Art Uploads 2024.5.1 https://github.com/ROpdebee/mb-userscripts')

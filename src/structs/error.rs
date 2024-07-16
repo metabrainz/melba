ae0c4538bb7209b8448f1135bbd772127a98b47c
yellowhatpro <yellowhatpro3119@gmail.com>
2024-07-16 11:49:34 +0530

    misc:error handling, interval inc of archiving (to match 12req/min)

--- src/structs/error.rs
+++ src/structs/error.rs
@@ -2,10 +2,10 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum ArchivalError {
-    #[error("request error")]
+    #[error("request error: {0}")]
     Request(#[from] reqwest::Error),
 
-    #[error("Could not deserialize error")]
+    #[error("Could not deserialize error: {0}")]
     JsonDeserialization(#[from] serde_json::Error),
 
     #[error("sqlx error: {0}")]

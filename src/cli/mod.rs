5c2fc7b9b050d5b0cc16d0861ff038780c446a88
RustyNova <rusty.nova.jsb@gmail.com>
2024-07-18 07:16:31 +0000

    refactor: cleanup code styling

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -9,79 +9,95 @@ mod utils;
 pub async fn start(pool: &PgPool) {
     let args = CliArgs::parse();
     match &args.command {
-        None => {
-            super::app::start(pool).await.unwrap();
-        }
         Some(Commands::QueueEditData { row_id }) => {
-            if row_id.is_none() {
+            // Argument check
+            // TODO: Concider making the argument mandatory by removing the `Option`?
+            let Some(row_id) = row_id else {
                 println!("{}", "Please pass row id".red());
                 return;
-            }
-            if let Ok(contains_urls) =
-                utils::insert_edit_data_row_to_internet_archive_urls(row_id.unwrap(), pool).await
-            {
-                if contains_urls {
-                    println!(
-                        "{} {}",
-                        "EditData's row enqueued successfully".green(),
-                        row_id.unwrap()
-                    )
-                } else {
-                    println!("{}", "No URLs found".red())
+            };
+
+            match utils::insert_edit_data_row_to_internet_archive_urls(*row_id, pool).await {
+                // We got urls!
+                Ok(true) => println!(
+                    "{} {}",
+                    "EditData's row enqueued successfully".green(),
+                    row_id
+                ),
+
+                // We don't have urls
+                Ok(false) => println!("{}", "No URLs found".red()),
+
+                // Error
+                Err(err) => {
+                    println!("{}", "Some error occurred".red());
+                    eprintln!("{err}");
                 }
-            } else {
-                println!("{}", "Some error occurred".red())
             }
         }
+
         Some(Commands::QueueEditNote { row_id }) => {
-            if row_id.is_none() {
+            // Argument check
+            // TODO: Concider making the argument mandatory by removing the `Option`?
+            let Some(row_id) = row_id else {
                 println!("{}", "Please pass row id".red());
                 return;
-            }
-            if let Ok(contains_urls) =
-                utils::insert_edit_note_row_to_internet_archive_urls(row_id.unwrap(), pool).await
-            {
-                if contains_urls {
-                    println!(
-                        "{} {}",
-                        "EditNote's row enqueued successfully".green(),
-                        row_id.unwrap()
-                    )
-                } else {
-                    println!("{}", "No URLs found".red())
+            };
+
+            match utils::insert_edit_note_row_to_internet_archive_urls(*row_id, pool).await {
+                // We got urls!
+                Ok(true) => println!(
+                    "{} {}",
+                    "EditNote's row enqueued successfully".green(),
+                    row_id
+                ),
+
+                // We don't have urls
+                Ok(false) => println!("{}", "No URLs found".red()),
+
+                // Error
+                Err(err) => {
+                    println!("{}", "Some error occurred".red());
+                    eprintln!("{err}");
                 }
-            } else {
-                println!("{}", "Some error occurred".red())
             }
         }
         Some(Commands::QueueURL { url }) => {
-            if url.is_none() {
+            // Argument check
+            // TODO: Concider making the argument mandatory by removing the `Option`?
+            let Some(url) = url else {
                 println!("{}", "Please pass URL".red());
                 return;
-            }
-            if let Ok(id) =
-                utils::insert_url_to_internet_archive_urls(url.clone().unwrap(), pool).await
-            {
-                println!(
+            };
+
+            match utils::insert_url_to_internet_archive_urls(url, pool).await {
+                Ok(id) => println!(
                     "{} {}",
                     "URL queued in internet_archive_urls, id: ".green(),
                     id
-                )
-            } else {
-                println!("{}", "Some error occurred".red())
+                ),
+
+                Err(err) => {
+                    println!("{}", "Some error occurred".red());
+                    eprintln!("{err}");
+                }
             }
         }
         Some(Commands::CheckStatus { job_id }) => {
-            if job_id.is_none() {
+            // Argument check
+            // TODO: Concider making the argument mandatory by removing the `Option`?
+            let Some(job_id) = job_id else {
                 println!("{}", "Please pass job id".red());
                 return;
-            }
-            println!("{:?}", job_id);
-            utils::get_job_id_status(job_id.clone().unwrap(), pool)
+            };
+
+            println!("Job id: {:?}", job_id);
+            utils::get_job_id_status(job_id.to_owned(), pool)
                 .await
                 .unwrap();
         }
-        Some(Commands::Poll) => {
+
+        Some(Commands::Poll) | None => {
             super::app::start(pool).await.unwrap();
         }
     }

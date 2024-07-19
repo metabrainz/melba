7572343cd0f24adcbb109c47b5845d3379bc96e9
yellowhatpro <yellowhatpro3119@gmail.com>
2024-07-19 11:50:14 +0530

    feat:added status check request for archival response containing job id

--- src/archival/error.rs
+++ src/archival/error.rs
@@ -5,7 +5,7 @@ pub enum ArchivalError {
     #[error("request error: {0}")]
     Request(#[from] reqwest::Error),
 
-    #[error("Could not deserialize error: {0}")]
+    #[error("Could not deserialize, error: {0}")]
     JsonDeserialization(#[from] serde_json::Error),
 
     #[error("sqlx error: {0}")]

59a5ea93c0ebbc35a78e1ee037ee4e678337d926
yellowhatpro <yellowhatpro3119@gmail.com>
2024-08-10 01:37:58 +0530

    feat:added last_unprocessed_rows population for dev mode in initdb.sh script

--- scripts/sql/002_CreateTable.sql
+++ scripts/sql/002_CreateTable.sql
@@ -14,21 +14,3 @@ CREATE TABLE external_url_archiver.last_unprocessed_rows (
         id_column           INTEGER,
         table_name          VARCHAR
 );
-
--- Insert the latest row from edit_data table to begin the poller
-INSERT INTO external_url_archiver.last_unprocessed_rows (id_column, table_name)
-SELECT edit, 'edit_data'
-FROM (
-    SELECT DISTINCT ON (edit) edit
-    FROM edit_data
-    ORDER BY edit  DESC LIMIT 1
-) AS latest_edit_data;
-
--- Insert the latest row from edit_note table to begin the poller
-INSERT INTO external_url_archiver.last_unprocessed_rows (id_column, table_name)
-SELECT id, 'edit_note'
-FROM (
-    SELECT DISTINCT ON (id) id
-    FROM edit_note
-    ORDER BY id  DESC LIMIT 1
-) AS latest_edit_note;

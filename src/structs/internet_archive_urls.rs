ae5583bd880c843dd7d37c3186057e7556d73e77
yellowhatpro <yellowhatpro3119@gmail.com>
2024-08-13 00:38:56 +0530

    ci: fix ci issues

--- src/structs/internet_archive_urls.rs
+++ src/structs/internet_archive_urls.rs
@@ -35,7 +35,9 @@ pub struct InternetArchiveUrls {
     pub id: i32,
     pub url: Option<String>,
     pub job_id: Option<String>,
+    #[allow(dead_code)]
     pub from_table: Option<String>,
+    #[allow(dead_code)]
     pub from_table_id: Option<i32>,
     pub created_at: Option<chrono::DateTime<chrono::Utc>>,
     pub retry_count: Option<i32>,
--- src/structs/last_unprocessed_row.rs
+++ src/structs/last_unprocessed_row.rs
@@ -4,5 +4,6 @@ use sqlx::FromRow;
 #[derive(Debug, Deserialize, FromRow)]
 pub struct LastUnprocessedRow {
     pub id_column: i32,
+    #[allow(dead_code)]
     pub table_name: String,
 }
